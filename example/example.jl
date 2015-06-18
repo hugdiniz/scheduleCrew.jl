af3306dbf83f886f0575a465c321bbcfdfea13a6
hugdiniz <hugdiniz@gmail.com>
2015-06-18 13:38:39 -0300

    creating types Model,Dataset, Crew and creating initial solution

--- example/example.jl
+++ example/example.jl
@@ -1,8 +1,17 @@
-cd("/home/hugdiniz/Work/Workspace/ScheduleCrew.jl/example")
+cd("/home/hugdiniz/Work/Workspace/scheduleCrew.jl/example")
 function readDataSet(path)
     return readcsv(path)
 end
 
-dataset = readDataSet("datasetModified/datasetModified.csv")
+dataMatrix = readDataSet("datasetModified/datasetModified.csv")
+dataset = Dataset(dataMatrix)
+numberCrews= 20
+model = Model(costCalc,dataset,st)
+
+
+function costCalc(edge,crew::Crew)
+
+
+end
 
 
